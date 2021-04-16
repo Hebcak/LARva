0b7e6a84a92002f83bc5a7d348cd82cb60975629
chlebajz <jeseper@seznam.cz>
2021-04-16 10:46:54 +0200

    Minpr changes and Technical review draft

--- SupplementalTools/Threshlod_picker_2.0.py
+++ SupplementalTools/Threshlod_picker_2.0.py
@@ -68,12 +68,12 @@ while True:
             approx = cv2.approxPolyDP(cnt, 0.02 * cv2.arcLength(cnt, True), True)
             if len(approx) != 4:  # not a square
                 cv2.drawContours(imgContour, cnt, -1, color_dict["Cyan"], 2)
-                imgContour = cv2.putText(imgContour, "Not a sloupek", (x, y - 3), cv2.FONT_HERSHEY_COMPLEX, 0.4, (0, 0, 0), 1)
+                imgContour = cv2.putText(imgContour, "Not a cone", (x, y - 3), cv2.FONT_HERSHEY_COMPLEX, 0.4, (0, 0, 0), 1)
                 continue
-            if float(h) / w > 8:  # standing Bannister
+            if float(h) / w > 8:  # standing Cone
                 imgContour = cv2.drawContours(imgContour, cnt, -1, color_dict[color], 2)
                 imgContour = cv2.putText(imgContour, color, (x, y - 3), cv2.FONT_HERSHEY_COMPLEX, 0.4, (0, 0, 0), 1)
-            elif float(w) / h > 8:  # Bannister on the floor
+            elif float(w) / h > 8:  # Cone on the floor
                 cv2.drawContours(imgContour, cnt, -1, color_dict["Yellow"], 2)
                 imgContour = cv2.putText(imgContour, "Fallen", (x, y - 3), cv2.FONT_HERSHEY_COMPLEX, 0.4, (0, 0, 0), 1)
             else:
